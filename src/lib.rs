a0c54dee4686fa9e6bde027a891412b27cc70379
Huon Wilson <dbau.pp+github@gmail.com>
2014-09-03 00:05:04 +1000

    Implement estimate_nth_prime & improve prime_pi with Dusart 1998.
    Also, convert estimate_prime_pi to take u64. This adds some small
    tables (< 2KB).

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 use std::{iter, cmp};
 use std::collections::{Bitv, bitv};
 
+#[allow(dead_code)]
+mod tables;
+
 /// Stores information about primes.
 pub struct Primes {
     // This only stores odd numbers, since even numbers are mostly
@@ -107,26 +110,106 @@ impl Primes {
     }
 }
 
-/// Returns estimated bounds for π(n), the number of primes less than
-/// or equal to `n`.
-///
-/// That is, if `(a, b) = estimate_prime_pi(n)`, `a ≤ π(n) ≤ b`.
-pub fn estimate_prime_pi(n: uint) -> (uint, uint) {
-    if n >= 55 {
-        let n = n as f64;
-        let lg = n.ln();
+// fixme track down Barkley & cite correctly.
 
-        ((n / (lg + 2.0)) as uint, (n / (lg - 4.0)) as uint)
-    } else {
-        static SMALL_PRIME_PI: [uint, .. 55] =
-            [0, 0, /*2*/1, /*3*/2, 2, /*5*/3, 3, /*7*/ 4, 4, 4,
-             4, /*11*/5, 5, /*13*/ 6, 6, 6, 6, /*17*/ 7, 7, /*19*/8,
-             8, 8, 8, /*23*/9, 9, 9, 9, 9, 9, /*29*/10,
-             10, /*31*/11, 11, 11, 11, 11, 11, /*37*/12, 12, 12,
-             12, /*41*/13, 13, /*43*/14, 14, 14, 14, /*47*/15, 15, 15,
-             15, 15, 15, /*53*/16, 16];
-        let x = SMALL_PRIME_PI[n];
+/// Returns estimated bounds for π(*n*), the number of primes less
+/// than or equal to `n`.
+///
+/// That is, if (*a*, *b*) = `estimate_prime_pi(n)`, *a* ≤ π(*n*) ≤
+/// *b*. The bounds used are proved in [1] and [2, Theorem 1.10].
+///
+/// [1]: Barkley Rosser (January 1941). "Explicit Bounds for Some
+/// Functions of Prime Numbers". American Journal of Mathematics 63
+/// (1): 211–232. doi:10.2307/2371291. JSTOR 2371291.
+///
+/// [2]: Dusart, Pierre. ["Autour de la fonction qui compte le nombre
+/// de nombres premiers."][pdf] PhD diss., Université de Limoges,
+/// 1998.
+///
+/// [pdf]: http://www.unilim.fr/laco/theses/1998/T1998_01.html
+pub fn estimate_prime_pi(n: u64) -> (u64, u64) {
+    if n < tables::SMALL_PRIME_PI.len() as u64 {
+        let x = tables::SMALL_PRIME_PI[n as uint] as u64;
         (x, x)
+    } else {
+        let n_ = n as f64;
+        let lg = n_.ln();
+        let inv_lg = 1.0 / lg;
+        let n_lg = n_ * inv_lg;
+
+        // numbers refer to parts of theorem 1.10 of [2].
+        let lo = if n >= 32299 {
+            // 6.
+            n_lg * (1.0 + inv_lg * (1.0 + 1.8 * inv_lg))
+        } else if n >= 5393 {
+            // 5.
+            n_ / (lg - 1.0)
+        } else if n >= 599 {
+            // 1.
+            n_lg * (1.0 + inv_lg)
+        } else {
+            // [1]
+            n_ / (lg + 2.0)
+        };
+
+        let hi = if n >= 13_220_000_000 {
+            // 3.
+            n_lg * (1.0 + 1.0992 * inv_lg)
+        } else if n >= 355991 {
+            // 7.
+            n_lg * (1.0 + inv_lg * (1.0 + 2.51 * inv_lg))
+        } else if n >= 60184 {
+            // 4.
+            n_ / (lg - 1.1)
+        } else {
+            // 2. (is this better than n/(log n - 4.0)?)
+            n_lg * (1.0 + 1.2762 * inv_lg)
+        };
+
+        (lo as u64, hi as u64)
+    }
+}
+
+/// Gives estimated bounds for *p<sub>n</sub>*, the `n`th prime number,
+/// 1-indexed (i.e. *p<sub>1</sub>* = 2, *p<sub>2</sub>* = 3).
+///
+/// That is, if (<i>a</i>,<i>b</i>) = `estimate_nth_prime(n)`, *a* ≤
+/// *p<sub>n</sub>* ≤ *b*. The bounds used are proved in [1], pages 31
+/// and 32.
+///
+/// [1]: Dusart, Pierre. ["Autour de la fonction qui compte le nombre
+/// de nombres premiers."][pdf] PhD diss., Université de Limoges, 1998.
+///
+/// [pdf]: http://www.unilim.fr/laco/theses/1998/T1998_01.html
+pub fn estimate_nth_prime(n: u64) -> (u64, u64) {
+    if n == 0 {
+        (0, 0)
+    } else if n <= tables::SMALL_PRIMES.len() as u64 {
+        // table is 0-indexed, n is 1-indexed, need to adjust.
+        let x = tables::SMALL_PRIMES[n as uint - 1] as u64;
+        (x, x)
+    } else {
+        let n_ = n as f64;
+        let lg = n_.ln();
+        let lglg = lg.ln();
+
+        let lo = lg + lglg - 1.0 + if n > 13196 {
+            // Theorem 1.6
+            (lglg - 2.25) / lg
+        } else {
+            0.0
+        };
+
+        let hi = lg + lglg + if n >= 39017 {
+            // Theorem 1.8
+            -0.9484
+        } else if n >= 27076 {
+            // Theorem 1.7
+            -1.0 + (lglg - 1.8) / lg
+        } else {
+            0.0
+        };
+        ((n_ * lo) as u64, (n_ * hi) as u64)
     }
 }
 
@@ -151,11 +234,11 @@ impl<'a> Iterator<uint> for PrimeIterator<'a> {
         // TODO: this doesn't run in constant time, is it super-bad?
         match (iter.next(), iter.next_back()) {
             (Some(lo), Some(hi)) => {
-                let (below_hi, above_hi) = estimate_prime_pi(hi);
-                let (below_lo, above_lo) = estimate_prime_pi(lo);
+                let (below_hi, above_hi) = estimate_prime_pi(hi as u64);
+                let (below_lo, above_lo) = estimate_prime_pi(lo as u64);
 
-                (below_hi - cmp::min(above_lo, below_hi),
-                 Some(above_hi - below_lo + 1))
+                ((below_hi - cmp::min(above_lo, below_hi)) as uint,
+                 Some((above_hi - below_lo + 1) as uint))
             }
             (Some(_), None) => (1, Some(1)),
             (None, _) => (0, Some(0))
@@ -184,8 +267,9 @@ impl<'a> DoubleEndedIterator<uint> for PrimeIterator<'a> {
 mod tests {
     extern crate test;
 
+    use std::num;
     use std::iter::range_step;
-    use super::{Primes, estimate_prime_pi};
+    use super::{Primes, estimate_prime_pi, estimate_nth_prime};
     use self::test::Bencher;
 
     #[test]
@@ -320,6 +404,46 @@ mod tests {
         for &(exponent, real) in sporadic.iter() {
             let n = num::pow(10, exponent);
             check(n, real);
+        }
+    }
+
+    #[test]
+    fn nth_prime() {
+        fn check(n: u64, p: u64) {
+            let (lo, hi) = estimate_nth_prime(n);
+            assert!(lo <= p && p <= hi,
+                    "found failing estimate at {}, should satisfy: {} <= {} <= {}",
+                    n, lo, p, hi);
+        }
+        let sieve = Primes::sieve(1_000_000);
+
+        for (i, p) in sieve.primes().enumerate() {
+            let n = i as u64 + 1;
+            check(n, p as u64);
+        }
+
+        let sporadic = [
+            (0, 2),
+            (1, 29),
+            (2, 541),
+            (3, 7919),
+            (4, 104729),
+            (5, 1299709),
+            (6, 15485863),
+            (7, 179424673),
+            (8, 2038074743),
+            (9, 22801763489),
+            (10, 252097800623),
+            (11, 2760727302517),
+            (12, 29996224275833),
+            (13, 323780508946331),
+            (14, 3475385758524527),
+            (15, 37124508045065437),
+            ];
+
+        for &(exponent, nth_prime) in sporadic.iter() {
+            let n = num::pow(10, exponent);
+            check(n, nth_prime);
         }
     }
 
