8f2d7289ee0f94d0382ec432c4ff010968baf3e3
Huon Wilson <dbau.pp+github@gmail.com>
2014-09-27 10:46:02 +1000

    Implement the Miller-Rabin primality test.
    Good for isolated/"rare" prime tests. (My tests indicate sieving and
    miller-rabin are about the same when doing 10000 tests upto 1000000.)

--- src/lib.rs
+++ src/lib.rs
@@ -189,7 +189,6 @@ impl Primes {
         }
         if n != 1 {
             let b = self.upper_bound();
-            println!("{} {} {}", b, b*b, n);
             if b * b >= n {
                 // n is not divisible by anything from 1...sqrt(n), so
                 // must be prime itself! (That is, even though we
@@ -292,9 +291,9 @@ pub fn estimate_nth_prime(n: u64) -> (u64, u64) {
         let lg = n_.ln();
         let lglg = lg.ln();
 
-        let lo = lg + lglg - 1.0 + if n > 13196 {
+        let lo = lg + lglg - 1.0 + if n > 3 {//13196 {
             // [2] Theorem 1.6
-            (lglg - 2.25) / lg
+            (lglg - 2.1) / lg
         } else {
             // [1] Theorem A (ii)
             0.0
@@ -318,6 +317,79 @@ pub fn estimate_nth_prime(n: u64) -> (u64, u64) {
         };
         ((n_ * lo) as u64, (n_ * hi) as u64)
     }
+}
+
+fn mod_exp(mut x: u64, mut d: u64, n: u64) -> u64 {
+    let mut ret = 1;
+    while d != 0 {
+        if d % 2 == 1 {
+            ret *= x;
+            if ret >= n {
+                ret %= n;
+            }
+        }
+        d /= 2;
+        x *= x;
+        if x >= n {
+            x %= n;
+        }
+    }
+    ret
+}
+
+/// Test if `n` is prime, using the deterministic version of the
+/// Miller-Rabin test.
+///
+/// Doing a lot of primality tests with numbers strictly below some
+/// upper bound will be faster using the `is_prime` method of a
+/// `Primes` instance.
+pub fn is_prime_miller_rabin(n: u64) -> bool {
+    static HINT: &'static [u64] = &[2];
+
+    // we have a strict upper bound, so we can just use the witness
+    // table of Pomerance, Selfridge & Wagstaff and Jeaschke to be as
+    // efficient as possible, without having to fall back to
+    // randomness.
+    static WITNESSES: &'static [(u64, &'static [u64])] =
+        [(2_046, HINT),
+         (1_373_652, &[2, 3]),
+         (9_080_190, &[31, 73]),
+         (25_326_000, &[2, 3, 5]),
+         (4_759_123_140, &[2, 7, 61]),
+         (1_112_004_669_632, &[2, 13, 23, 1662803]),
+         (2_152_302_898_746, &[2, 3, 5, 7, 11]),
+         (3_474_749_660_382, &[2, 3, 5, 7, 11, 13]),
+         (341_550_071_728_320, &[2, 3, 5, 7, 11, 13, 17]),
+         (0xFFFF_FFFF_FFFF_FFFF, &[2, 3, 5, 7, 11, 13, 17, 19, 23])
+         ];
+
+    if n % 2 == 0 { return n == 2 }
+    if n == 1 { return false }
+
+    let mut d = n - 1;
+    let mut s = 0u;
+    while d % 2 == 0 { d /= 2; s += 1 }
+
+    let witnesses =
+        WITNESSES.iter().find(|&&(hi, _)| hi >= n)
+            .map(|&(_, wtnss)| wtnss).unwrap();
+    'next_witness: for &a in witnesses.iter() {
+        let mut power = mod_exp(a, d, n);
+        if power == 1 { continue 'next_witness }
+
+        for _r in range(0, s) {
+            if power == n - 1 {
+                continue 'next_witness
+            }
+            power *= power;
+            if power >= n {
+                power %= n;
+            }
+        }
+        return false
+    }
+
+    true
 }
 
 impl<'a> Iterator<uint> for PrimeIterator<'a> {
@@ -376,7 +448,8 @@ mod tests {
 
     use std::num;
     use std::iter::range_step;
-    use super::{Primes, estimate_prime_pi, estimate_nth_prime};
+    use super::{Primes, estimate_prime_pi, estimate_nth_prime,
+                is_prime_miller_rabin};
     use self::test::Bencher;
 
     #[test]
@@ -634,6 +707,19 @@ mod tests {
         }
     }
 
+    #[test]
+    fn miller_rabin() {
+        static LIMIT: uint = 1_000_000;
+        let sieve = Primes::sieve(LIMIT);
+        for x in range(0, LIMIT) {
+            let s = sieve.is_prime(x);
+            let mr = is_prime_miller_rabin(x as u64);
+
+            assert!(s == mr, "miller_rabin {} mismatches sieve {} for {}",
+                    mr, s, x)
+        }
+    }
+
     #[bench]
     fn sieve_small(b: &mut Bencher) {
         b.iter(|| Primes::sieve(100))
@@ -659,4 +745,23 @@ mod tests {
     fn iterate_small(b: &mut Bencher) { bench_iterate(b, 100) }
     #[bench]
     fn iterate_large(b: &mut Bencher) { bench_iterate(b, 100_000) }
+
+
+    static N: uint = 1_000_000;
+    static STEP: uint = 101;
+    #[bench]
+    fn bench_miller_rabin_tests(b: &mut Bencher) {
+        b.iter(|| {
+            range_step(1, N, STEP)
+                .filter(|&n| is_prime_miller_rabin(n as u64)).count()
+        })
+    }
+    #[bench]
+    fn bench_sieve_tests(b: &mut Bencher) {
+        b.iter(|| {
+            let sieve = Primes::sieve(1_000_000);
+            range_step(1, N, STEP)
+                .filter(|&n| sieve.is_prime(n)).count()
+        })
+    }
 }
