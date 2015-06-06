ea26e9b5c27e4420081f9dc4ccb51c935d267147
Huon Wilson <dbau.pp+github@gmail.com>
2015-06-06 14:36:35 +1000

    sieve: add pointer black-boxes.

--- primal-sieve/src/lib.rs
+++ primal-sieve/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(feature = "unstable", feature(asm))]
 #![cfg_attr(all(test, feature = "unstable"), feature(test))]
 
 #[cfg(all(test, feature = "unstable"))] extern crate test;
@@ -5,6 +6,23 @@
 extern crate primal_bit;
 extern crate primal_estimate;
 extern crate hamming;
+
+// black boxes for pointers; LLVM isn't so happy without
+// them. Unfortunately only usable with unstable, but the code isn't
+// *too* much slower without them.
+#[cfg(feature = "unstable")]
+#[inline(always)]
+fn b<T>(mut p: *mut T) -> *mut T { unsafe { asm!("": "+r"(p)) } p }
+#[cfg(not(feature = "unstable"))]
+#[inline(always)]
+fn b<T>(p: *mut T) -> *mut T { p }
+
+#[cfg(feature = "unstable")]
+#[inline(always)]
+fn b_imm<T>(mut p: *const T) -> *const T { unsafe { asm!("": "+r"(p)) } p }
+#[cfg(not(feature = "unstable"))]
+#[inline(always)]
+fn b_imm<T>(p: *const T) -> *const T { p }
 
 mod streaming;
 pub use streaming::StreamingSieve;
