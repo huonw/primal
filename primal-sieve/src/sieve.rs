d51b938d555407f883df2a56623bd68959634988
Huon Wilson <dbau.pp+github@gmail.com>
2015-06-07 00:49:41 +1000

    sieve: implement `primes_from` iterator.

--- primal-sieve/src/sieve.rs
+++ primal-sieve/src/sieve.rs
@@ -113,6 +113,112 @@ impl Sieve {
             }
         }
     }
+
+    /// Return an iterator over the primes from `n` (inclusive) to the
+    /// end of this sieve.
+    ///
+    /// NB. it is not guaranteed that the end is the same as the limit
+    /// passed to `new`: it may be larger. Consider using `take_while`
+    /// if the limit must be exact.
+    ///
+    /// # Panics
+    ///
+    /// If `n` is out of range (greater than `self.upper_bound()`),
+    /// `count_upto` will panic.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # extern crate primal;
+    /// let sieve = primal::Sieve::new(1_000);
+    /// for p in sieve.primes_from(100).take_while(|x| *x <= 1000) {
+    ///     println!("{}", p);
+    /// }
+    /// ```
+    pub fn primes_from<'a>(&'a self, n: usize) -> PrimesFrom<'a> {
+        assert!(n <= self.upper_bound());
+        let early = match n {
+            0...2 => Early::Two,
+            3 => Early::Three,
+            4...5 => Early::Five,
+            _ => Early::Done
+        };
+        let (_, base, tweak) = self.index_for(n);
+        let (tweak_u64, tweak_bit) = (tweak / 64, tweak % 64);
+        let tweak_mask = (!0) << tweak_bit;
+        assert!(self.seen.len() == 1 || self.seen[0].len() % 64 == 0);
+        let base_u64_count = base * self.seen[0].len() / 64 + tweak_u64;
+
+        let mut elems = self.seen[base].as_u64s()[tweak_u64..].iter();
+        let current = !elems.next().unwrap() & tweak_mask;
+
+        PrimesFrom {
+            early: early,
+            base: base_u64_count * ITER_BASE_STEP,
+            current: current,
+            elems: elems,
+            bits: self.seen[base + 1..].iter(),
+        }
+    }
+}
+
+use std::slice;
+const ITER_BASE_STEP: usize = 8 * wheel::BYTE_MODULO;
+
+enum Early {
+    Two,
+    Three,
+    Five,
+    Done,
+}
+
+pub struct PrimesFrom<'a> {
+    early: Early,
+    base: usize,
+    current: u64,
+    elems: slice::Iter<'a, u64>,
+    bits: slice::Iter<'a, BitVec>,
+}
+
+impl<'a> Iterator for PrimesFrom<'a> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        match self.early {
+            Early::Done => {}
+            Early::Two => {
+                self.early = Early::Three;
+                return Some(2)
+            }
+            Early::Three => {
+                self.early = Early::Five;
+                return Some(3)
+            }
+            Early::Five => {
+                self.early = Early::Done;
+                return Some(5)
+            }
+        }
+        let mut c = self.current;
+        'find_c: while c == 0 {
+            for &next in &mut self.elems {
+                self.base += ITER_BASE_STEP;
+                let next = !next;
+                if next != 0 {
+                    c = next;
+                    break 'find_c
+                }
+            }
+            match self.bits.next() {
+                Some(bits) => self.elems = bits.as_u64s().iter(),
+                None => return None,
+            }
+        }
+
+        let lsb = c.trailing_zeros();
+        self.current = c & (c - 1);
+        Some(self.base + wheel::TRUE_AT_BIT_64[lsb as usize])
+    }
 }
 
 #[cfg(test)]
@@ -132,6 +238,45 @@ mod tests {
         }
     }
 
+    #[test]
+    fn primes_from_smoke() {
+        let limit = 100;
+        let primes = Sieve::new(limit);
+        let real = &[2, 3, 5, 7, 11,
+                     13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47,
+                     53, 59, 61, 67, 71,
+                     73, 79, 83, 89, 97];
+        for i in 0..limit {
+            let idx = real.iter().position(|x| *x >= i).unwrap_or(real.len());
+            assert_eq!(primes.primes_from(i).take_while(|x| *x <= limit).collect::<Vec<_>>(),
+                       &real[idx..]);
+        }
+    }
+    #[test]
+    fn primes_from_count() {
+        let limit = 2_100_000;
+        let primes = Sieve::new(limit);
+
+        let upto = 2_000_000;
+        assert_eq!(primes.primes_from(0).take_while(|x| *x <= upto).count(),
+                   primes.count_upto(upto));
+    }
+    #[test]
+    fn primes_from_equality() {
+        let limit = 2_000_000;
+        let primes = Sieve::new(limit);
+        let real = Primes::sieve(limit);
+
+        let real = real.primes().take_while(|x| *x <= limit);
+        let computed = primes.primes_from(0).take_while(|x| *x <= limit);
+        let mut i = 0;
+        for (r, p) in real.zip(computed) {
+            assert_eq!(r, p);
+            i += 1;
+        }
+        assert_eq!(i, primes.count_upto(limit));
+    }
     #[test]
     fn upper_bound() {
         for i in 1..1000 {
@@ -197,4 +342,19 @@ mod benches {
     fn count_upto_large(b: &mut Bencher) { count_upto(b, 100_000) }
     #[bench]
     fn count_upto_huge(b: &mut Bencher) { count_upto(b, 10_000_000) }
+
+    fn bench_iterate(b: &mut Bencher, upto: usize) {
+        let sieve = Sieve::new(upto);
+
+        b.iter(|| {
+            sieve.primes_from(0).count()
+        })
+    }
+
+    #[bench]
+    fn iterate_small(b: &mut Bencher) { bench_iterate(b, 100) }
+    #[bench]
+    fn iterate_large(b: &mut Bencher) { bench_iterate(b, 100_000) }
+    #[bench]
+    fn iterate_huge(b: &mut Bencher) { bench_iterate(b, 10_000_000) }
 }
