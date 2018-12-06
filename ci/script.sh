4c5f7beb99f68eb5a4806113f8225661b24146d8
Huon Wilson <wilson.huon@gmail.com>
2018-12-06 23:51:37 +1100

    ci: add -v to all cargo invocations.

--- ci/script.sh
+++ ci/script.sh
@@ -9,25 +9,25 @@ if [ ! -z "$UNSTABLE" ]; then
 fi
 if [ ! -z "$TARGET" ]; then
     rustup target add "$TARGET"
-    cargo install cross --force
+    cargo install -v cross --force
     cargo="cross"
     target_param="--target $TARGET"
 fi
 
-$cargo build --all $target_param --features "$features"
+$cargo build -v --all $target_param --features "$features"
 if [ "$TRAVIS_RUST_VERSION" = "1.20.0" ]; then
     # unfortunately, testing requires building dev-deps, which
     # requires a newer rustc than this.
     exit 0
 fi
 
-$cargo test --all $target_param --features "$features"
-$cargo bench --all $target_param --features "$features" -- --test # don't actually record numbers
-$cargo doc --all $target_param --features "$features"
+$cargo test -v --all $target_param --features "$features"
+$cargo bench -v --all $target_param --features "$features" -- --test # don't actually record numbers
+$cargo doc -v --all $target_param --features "$features"
 
-$cargo test -p primal-sieve --features "$features primal-sieve/safe"
+$cargo test -v -p primal-sieve --features "$features primal-sieve/safe"
 
-$cargo test --all --release --features "$features primal-sieve/slow_tests primal-slowsieve/slow_tests"
+$cargo test -v --all --release --features "$features primal-sieve/slow_tests primal-slowsieve/slow_tests"
 
 if [ ! -z "$COVERAGE" ]; then
     if [ ! -z "$TARGET" ]; then
@@ -35,7 +35,7 @@ if [ ! -z "$COVERAGE" ]; then
         exit 1
     fi
 
-    cargo install cargo-travis --force
-    cargo coverage --all -m coveralls_results --kcov-build-location "$PWD/target" --features "$features"
+    cargo install -v cargo-travis --force
+    cargo coverage -v --all -m coveralls_results --kcov-build-location "$PWD/target" --features "$features"
     "$PWD/target/kcov-master/build/src/kcov" --coveralls-id="$TRAVIS_JOB_ID" coverage_results --merge coverage_results
 fi
