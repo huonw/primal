e78d1d56af4dd5b3c409d6fd6b4df2808bceb47e
Kaimyn Chapman <kchap21@hotmail.com>
2019-10-19 11:08:03 +0100

    Add some benchmarks

--- primal-count/benches/bench.rs
+++ primal-count/benches/bench.rs
@@ -4,6 +4,9 @@ extern crate bencher;
 use bencher::Bencher;
 
 use primal_count::primes_below;
+use primal_count::PrimeCounter;
+
+const LARGE_VAL: usize = 10_000_000_000;
 
 fn a(bench: &mut Bencher) {
     bench.iter(|| {
@@ -19,9 +22,45 @@ fn b(bench: &mut Bencher) {
 
 fn c(bench: &mut Bencher) {
     bench.iter(|| {
-        primes_below(10_000_000_000)
+        primes_below(LARGE_VAL)
     });
 }
 
-benchmark_group!(benches, a, b, c);
+fn d(bench: &mut Bencher) {
+    bench.iter(|| {
+        let mut pc = PrimeCounter::new(LARGE_VAL);
+        pc.primes_below(LARGE_VAL);
+    });
+}
+
+fn e(bench: &mut Bencher) {
+    bench.iter(|| {
+        primes_below(LARGE_VAL);
+        primes_below(LARGE_VAL >> 4);
+    });
+}
+
+fn f(bench: &mut Bencher) {
+    bench.iter(|| {
+        let mut pc = PrimeCounter::new(LARGE_VAL);
+        pc.primes_below(LARGE_VAL);
+        pc.primes_below(LARGE_VAL >> 4);
+    });
+}
+
+fn g(bench: &mut Bencher) {
+    bench.iter(|| {
+        primes_below(LARGE_VAL * 100);
+    });
+}
+
+fn h(bench: &mut Bencher) {
+    bench.iter(|| {
+        primes_below(1_000_000_000_000_000);
+    });
+}
+
+
+
+benchmark_group!(benches, a, b, c, d, e, f, g, h);
 benchmark_main!(benches);
