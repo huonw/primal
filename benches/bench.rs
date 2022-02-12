48389aa913c5b6ef04a0c192a83e06245ce35bbc
Josh Stone <cuviper@gmail.com>
2022-02-11 18:08:58 -0800

    Migrate from criterion deprecations

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,39 +1,36 @@
 #[macro_use]
 extern crate criterion;
-use criterion::{Criterion, Fun};
+use criterion::Criterion;
 
 const N: usize = 1_000_000;
 const STEP: usize = 101;
 
 fn is_prime(c: &mut Criterion) {
-    let miller_rabin = Fun::new(
-        "is_prime",
-        |b, _| {
-            b.iter(|| (1..N).step_by(STEP).filter(|&n| primal::is_prime(n as u64)).count())
-        });
-    let sieve = Fun::new(
-        "Sieve::is_prime",
-        |b, _| {
-            let sieve = primal::Sieve::new(N);
-            b.iter(|| {
-                (1..N).step_by(STEP)
-                    .filter(|&n| sieve.is_prime(n)).count()
-            })
-        });
-    let sieve_with_init = Fun::new(
-        "Sieve::is_prime with init",
-        |b, _| {
-            b.iter(|| {
-                let sieve = primal::Sieve::new(N);
-                (1..N).step_by(STEP)
-                    .filter(|&n| sieve.is_prime(n)).count()
-            })
-        });
+    let mut group = c.benchmark_group("is_prime");
 
-    c.bench_functions(
-        "is_prime", vec![miller_rabin, sieve, sieve_with_init], ());
+    group.bench_function("is_prime", |b| {
+        b.iter(|| {
+            (1..N)
+                .step_by(STEP)
+                .filter(|&n| primal::is_prime(n as u64))
+                .count()
+        })
+    });
+
+    group.bench_function("Sieve::is_prime", |b| {
+        let sieve = primal::Sieve::new(N);
+        b.iter(|| (1..N).step_by(STEP).filter(|&n| sieve.is_prime(n)).count())
+    });
+
+    group.bench_function("Sieve::is_prime with init", |b| {
+        b.iter(|| {
+            let sieve = primal::Sieve::new(N);
+            (1..N).step_by(STEP).filter(|&n| sieve.is_prime(n)).count()
+        })
+    });
+
+    group.finish();
 }
 
 criterion_group!(benches, is_prime);
 criterion_main!(benches);
-
